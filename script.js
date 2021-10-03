c7382749e23e4b83bdc12cf24b4eb6338c68fd3c
nilberm <nilber16@outlook.com>
2021-10-03 10:17:29 -0300

    E-mail validation done

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
+function validationEmail(){
+    var email = document.getElementById('email')
+    var error = document.getElementById('error')
 
+    if(!email.checkValidity()){
+        error.style.display = "block"
+    }
+}
+
+function redefinirMsg(){
+    var error = document.getElementById('error')
+    if(error.style.display == "block"){
+        error.style.display = "none"
+    }
+}
 
